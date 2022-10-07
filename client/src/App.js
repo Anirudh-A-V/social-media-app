5fadd4f4a1d7c23e04d56e457815d220b28d234c
Anirudh A V <anirudh.av02@gmail.com>
2022-10-08 00:35:59 +0530

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import useStyles from './styles';
+import ClubHub from './images/ClubHub.png'
 
 const App = () => {
+    const classes = useStyles();
+
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant='h2' align='center'>
+          ClubHub
+        </Typography>
+        <img className={classes.image} src={ClubHub} alt="ClubHub" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
