6002875ad32f1a2da6d0622c029a27a336a9e788
Anirudh A V <anirudh.av02@gmail.com>
2022-10-07 23:05:22 +0530

    Backend initialised > MongoDB connection established > Environment variables initialised and loaded > Server started successfully

--- server/index.js
+++ server/index.js
@@ -2,9 +2,24 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+dotenv.config();
+
+// MongoDB connection
+
+const CONNECTION_URL = process.env.MONGO_CONNECTION_URL;
+// const CONNECTION_URL = 'mongodb+srv://Anirudh-A-V:Anirudh%402002911@socialmediacuster1.ghamwo6.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
